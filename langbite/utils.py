160ae9a70f25124a5c5d2c9f7204526d8b368695
mgv99 <gomezmarcos99@gmail.com>
2024-04-18 17:02:31 +0200

    Added cleaning values for some specific LLMs

--- langbite/utils.py
+++ langbite/utils.py
@@ -1,4 +1,17 @@
 import re
 
 def clean_string(input: str):
-    return re.sub('\n',' ',input.strip()).lower()
+    cleaned_value = re.sub('\n',' ',input.strip()).lower()
+    # Fixing mistralai/Mistral-7B-v0.1 outputs
+    cleaned_value = re.sub('## expert answer', '', cleaned_value.strip()).lower()
+    cleaned_value = re.sub('## answer', '', cleaned_value.strip()).lower()
+    cleaned_value = re.sub('## expert answeranswer:', '', cleaned_value.strip()).lower()
+    cleaned_value = re.sub('## expert answer answer:', '', cleaned_value.strip()).lower()
+    cleaned_value = re.sub('##', '', cleaned_value.strip()).lower()
+    cleaned_value = re.sub('answer yes', 'yes', cleaned_value.strip()).lower()
+    cleaned_value = re.sub('answeryes', 'yes', cleaned_value.strip()).lower()
+    cleaned_value = re.sub('answerno', 'no', cleaned_value.strip()).lower()
+    cleaned_value = re.sub('answer no', 'no', cleaned_value.strip()).lower()
+    return cleaned_value
+
+
